96d58dee41f2f6ee308aca60b4a5d7f0aefa4af5
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-22 09:17:09 +0000

    Started working on the homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,8 @@
 const Home = () => {
   return (
     <section className="full-width flex-center flex-col">
-        <h1 className="text-center head-text">AI-Prompt Connect</h1>
-        
+        <h1 className="text-center head_text">AI-Prompt Connect</h1>
+
     </section>
   )
 }
