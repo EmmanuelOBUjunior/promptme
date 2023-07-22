50e5a21a38723a80f05a6853db1165cbab5233eb
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-22 09:32:10 +0000

    Working on the homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,12 @@
 const Home = () => {
   return (
     <section className="full-width flex-center flex-col">
-        <h1 className="text-center head_text">AI-Prompt Connect:
+        <h1 className="text-center head_text">PromptMe:
         <br className="max-md:hidden"/>
         <span className="indigo_gradient text center">Empowering Minds, Inspiring Creativity</span>
         </h1>
         <p className="text-center">
-
+        PromptMe is a revolutionary app that brings together aspiring writers, content creators, and AI enthusiasts. Discover a vast library of creative AI prompts generated to spark your imagination and unleash your potential.
         </p>
     </section>
   )
