77e8e3da45750d99bc939e4c3ea1811aedd76222
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-22 10:07:28 +0000

    Importing packages, Navbar component

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image'
 import { useState, useEffect } from 'react';
-import {} from 'next-auth/react'
+import { signIn, signOut, getProviders, useSession} from 'next-auth/react'
 
 const Nav = () => {
   return (
