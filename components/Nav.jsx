489d21e5231acd8182c7e57e0b47fc05efd52e62
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-22 10:12:31 +0000

    Importing packages, Navbar component

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,9 +8,14 @@ import { signIn, signOut, getProviders, useSession} from 'next-auth/react'
 const Nav = () => {
   return (
     <nav className='w-full flex-between pt-3 mb-16'>
+      <Link href='/' className='flex gap-2 flex-center'>
     <Image
     src = 'assets/images/logo.svg'
+    width = {50}
+    height={50}
+    alt='Prompt Me Logo'
     />
+      </Link>
     </nav>
   )
 }
