f74736dda50c23c9ded32431b1c639725e0e413f
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-22 09:33:21 +0000

    Working on the homepage

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ const Home = () => {
         <br className="max-md:hidden"/>
         <span className="indigo_gradient text center">Empowering Minds, Inspiring Creativity</span>
         </h1>
-        <p className="text-center">
+        <p className=" desc text-center">
         PromptMe is a revolutionary app that brings together aspiring writers, content creators, and AI enthusiasts. Discover a vast library of creative AI prompts generated to spark your imagination and unleash your potential.
         </p>
     </section>
