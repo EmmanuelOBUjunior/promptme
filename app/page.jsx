4791719814c6d204680a4eda613a0836553142f0
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-22 09:26:59 +0000

    Change orange gradient

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ const Home = () => {
     <section className="full-width flex-center flex-col">
         <h1 className="text-center head_text">AI-Prompt Connect:</h1>
         <br className="max-md:hidden"/>
-        <span className="orang">Empowering Minds, Inspiring Creativity</span>
+        <span className="indigo-gradient">Empowering Minds, Inspiring Creativity</span>
     </section>
   )
 }
