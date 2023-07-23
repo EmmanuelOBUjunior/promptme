3ec24e304a76a963f26b61c6a1b148748dfc6fb4
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-23 17:12:31 +0000

    Setting up database

--- utils/database.js
+++ utils/database.js
@@ -1,3 +1,12 @@
 import mongoose from "mongoose";
 
 let isConnected = false;
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true)
+
+    if(isConnected) {
+        console.log("Database is already connected")
+        return;
+    }
+}
