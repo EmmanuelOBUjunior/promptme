6f588a1948fb228c17881f1d1e345765513aa49a
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-23 17:57:07 +0000

    Setting up user model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,15 @@
 import { Schema, model, models } from "mongoose";
+
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        unique: [true, "Email already exists"],
+        required: [true, "Email is required"],
+    },
+    username: {
+        type: String,
+        required: [true, "Username already exists"],
+        match: 
+    }
+})
