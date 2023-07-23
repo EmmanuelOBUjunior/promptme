3ece4c9ee3fd130273a0229fceda1666bf3c1f2c
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-23 16:35:54 +0000

    Wrapped Provider around layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import Nav from "@components/Nav"
+import Provider from "@components/Provider"
 import "@styles/globals.css"
 
 
@@ -10,6 +11,7 @@ const RootLayout = ({children}) => {
   return (
     <html lang="en">
         <body>
+            <Provider>
             <div className="main">
                 <div className="gradient">    
                 </div>
@@ -19,6 +21,7 @@ const RootLayout = ({children}) => {
                 <Nav/>
                 {children}
             </main>
+            </Provider>
         </body>
     </html>
   )
