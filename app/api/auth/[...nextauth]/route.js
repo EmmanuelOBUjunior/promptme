d7c404067f3bc5cf27dc3f0601c04935e04a3704
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-23 18:26:42 +0000

    Imported User to api route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google'
 import { connectToDB } from "@utils/database";
+import User from "@models/User";
 
 
 const handler = NextAuth({
