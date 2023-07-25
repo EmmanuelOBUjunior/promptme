e0556988b5eedea0dbf8fe5d6a44739cfa4bd26b
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-25 07:08:03 +0000

    Working on forms page

--- components/Form.jsx
+++ components/Form.jsx
@@ -4,7 +4,7 @@ const Form = ({type, post, submitting, hanldesubmit, setPost}) => {
   return (
     <section className="w-full max-w-full flex-start flex-col">
       <h1 className='head-text text-left'>
-        span.
+        <span className="blue_gradient">{type} Post</span>
       </h1>
     </section>
   )
