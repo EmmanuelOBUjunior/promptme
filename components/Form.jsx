c7105d3afe869f792ad98f9b11bbca38cc1c35a4
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-25 07:24:49 +0000

    Working on form's page

--- components/Form.jsx
+++ components/Form.jsx
@@ -4,9 +4,9 @@ const Form = ({type, post, submitting, hanldesubmit, setPost}) => {
   return (
     <section className="w-full max-w-full flex-start flex-col">
       <h1 className='head_text text-left'>
-        <span className="blue_gradient">{type} Post</span>
+        <span className="amber_gradient">{type} Post</span>
       </h1>
-      <p className='desc text-left max-w-md'>Unleash creativity and explore limitless possibilities with by {type}ing and sharing your masterpieces with a thrving community of creators. Inspiring prompts tailored to your interests.</p>
+      <p className='desc text-left max-w-md'>Unleash creativity and explore limitless possibilities with by creating and sharing your masterpieces with a thriving community of creators. Inspiring prompts tailored to your interests.</p>
     </section>
   )
 }
