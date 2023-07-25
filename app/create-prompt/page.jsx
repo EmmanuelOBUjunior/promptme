be01a331997e66d0b5975537a71ecfcfbab35a59
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-25 06:45:09 +0000

    Working on the create post page

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,9 +1,13 @@
+'use client'
+
 import { useSession } from "next-auth/react"
 import { useState } from "react"
 import { useRouter } from "next/router"
 import Form from "@components/Form"
 
 const CreatePost = () => {
+    const [submitting, setsubmitting] = useState(false)
+
   return (
     <Form/>
   )
