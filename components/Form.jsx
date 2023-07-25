ff64a32bd298d240886e6a75118a6f196a63d78c
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-25 07:11:02 +0000

    Working on forms page

--- components/Form.jsx
+++ components/Form.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const Form = ({type, post, submitting, hanldesubmit, setPost}) => {
   return (
     <section className="w-full max-w-full flex-start flex-col">
-      <h1 className='head-text text-left'>
+      <h1 className='head_text text-left'>
         <span className="blue_gradient">{type} Post</span>
       </h1>
+      <p className='desc text-left max-w-md'></p>
     </section>
   )
 }
