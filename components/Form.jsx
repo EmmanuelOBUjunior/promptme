30ba7f6190197ded9dedd9012e8beef4e688f054
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-26 07:31:26 +0000

    Working on the create post page

--- components/Form.jsx
+++ components/Form.jsx
@@ -35,7 +35,7 @@ const Form = ({type, post, submitting, hanldesubmit, setPost}) => {
       />
       </label>
       <div className="flex-end mx-3 mb-5 gap-4">
-        <Link href= "/" className='text-gray-500 test-sm'>Cancel</Link>
+        <Link href= "/" className='text-gray-500 text-sm'>Cancel</Link>
       </div>
       </form>
     </section>
