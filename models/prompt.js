0a2ea4bda3d41f45deb7b86f9006f84826e20317
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-27 08:10:58 +0000

    Working on prompt model

--- models/prompt.js
+++ models/prompt.js
@@ -1,1 +1,19 @@
 import {Schema, models, model} from "@models/prompt"
+
+const PromptSchema = new Schema({
+    author : {
+        type: Schema.Types.ObjectID,
+        ref: "User"
+    },
+    prompt:{
+        type: String,
+        required: [true, 'Prompt is required']
+    },
+    tag : {
+        type: String,
+        required: [true, 'Tag is required']
+    }
+});
+
+const Prompt = models.Prompt || models('Prompt', PromptSchema);
+export default Prompt;
