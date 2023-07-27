7d97fabcfae767221bb9fdabb8dffccb3834c8a8
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-27 08:23:20 +0000

    Working on prompt api

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -5,7 +5,7 @@ export const POST = async (req) =>{
     const {userId, prompt, tag} = await req.json();
 
     try{
-        
+        await connectToDB()
     }catch(error){
         console.log(error)
     }
