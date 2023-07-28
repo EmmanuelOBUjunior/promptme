4500f14a1cfeb6408fed88979b43f223f5e81279
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-28 07:59:10 +0000

    Working on prompt api route

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -16,7 +16,7 @@ export const POST = async (req) =>{
         return new Response(JSON.stringify(newPrompt), {status:201})
 
     }catch(error){
-        console.log(error)
+        return new Response("Falied to create a new prompt", {status: 500}
     }
 
 }
