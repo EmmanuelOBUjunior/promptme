8b7dedd7e64186f7f00eec5d66c480893821a61e
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-07-31 16:30:14 +0000

    Fixing router issue

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -7,7 +7,7 @@ export const POST = async (req) =>{
     try{
         await connectToDB();
         const newPrompt = new Prompt({
-            author: userId,
+            creator: userId,
             prompt,
             tag
         })
