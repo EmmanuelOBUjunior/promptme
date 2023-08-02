a427805388c55837da6375b49867400df7fd00b5
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-02 13:57:16 +0000

    Working on route to home after sign out

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import {userRouter} from 'react/navigation';
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
