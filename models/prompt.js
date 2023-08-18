0eacae6fb08cf67712f12896b563b55fca48b266
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-18 18:23:49 +0000

    Solving creating API issue

--- models/prompt.js
+++ models/prompt.js
@@ -1,8 +1,8 @@
-import {Schema, models, model} from "mongoose"
+import {Schema, model, models} from "mongoose"
 
 const PromptSchema = new Schema({
     creator : {
-        type: Schema.Types.ObjectID,
+        type: Schema.Types.ObjectId,
         ref: "User"
     },
     prompt:{
