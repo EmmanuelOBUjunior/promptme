b482551d3075506b8b71bf0954e3b4e3e3812c5b
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-18 19:42:06 +0000

    Creating get prompt api

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -10,7 +10,7 @@ const PromptCardList = ({data, handleTagClick}) =>{
   return(
     <div className='mt-16 prompt_layout'>
       {data.map((post)=>{
-        <PostCard
+        <PromptCard
         key = {post.id}
         post={post}
         handleTagClick = {handleTagClick}
