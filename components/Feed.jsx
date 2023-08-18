b68df950cf9e91b1a7243d707d6ab39fa7ed268e
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-18 19:03:36 +0000

    Working on feed component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import {useState} from 'react'
 
 const Feed = () => {
+  u
+
   return (
     <section className='feed'>
       <form className='relative w-full flex-center'>
