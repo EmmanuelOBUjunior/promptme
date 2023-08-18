57ec2d317fc51804998d71bcd0446aa80e231059
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-18 19:05:13 +0000

    Working on feed component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 const Feed = () => {
   const [searchText, setSearchText] = useState("")
