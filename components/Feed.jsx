80314d8539e0b7d8c886d536687c3df25700c438
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-18 19:03:01 +0000

    Working on feed component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -6,6 +6,8 @@ const Feed = () => {
       <form className='relative w-full flex-center'>
         <input 
         type="text"
+        value = {searchText}
+        onChange={handleSearchChange}
         className='search_input peer'
         placeholder='Search for a tag or username or author'
         />
