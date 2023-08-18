91bd6aec0739ae82c8f87df9f7cde47b9bfdc5fa
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-18 18:15:03 +0000

    Solving creating API issue

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -16,7 +16,7 @@ const CreatePrompt = () => {
     })
 
     const createPrompt = async (e) =>{
-        // e.preventDefault()
+        e.preventDefault()
         setSubmitting(true)
 
         try{
