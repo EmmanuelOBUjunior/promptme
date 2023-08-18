f014181f6b1bbdc4ff97bf6362f0835fb01a8daf
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-18 19:06:32 +0000

    Working on feed component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,6 +3,8 @@
 import React from 'react'
 import {useState, useEffect} from 'react'
 
+import PostCard from './PostCard'
+
 const Feed = () => {
   const [searchText, setSearchText] = useState("")
 
