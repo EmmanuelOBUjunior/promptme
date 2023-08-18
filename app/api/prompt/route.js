9f7e1c621d9a56f83237e4558597fe6ef66ceba9
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-18 19:36:47 +0000

    Creating get prompt api

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -6,6 +6,8 @@ export const GET = async (req) =>{
     try {
         await connectToDB()
         const prompts = await Prompt.find({}).populate('creator')
+
+        return new Response(JSON.stringify(prompts), {status: 200})
     } catch (error) {
         
     }
