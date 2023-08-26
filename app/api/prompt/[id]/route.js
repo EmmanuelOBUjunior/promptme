e3cccaf568a32fe35e5a94f3e5bf32cc88b6da6a
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-26 19:37:17 +0000

    Working on prompts api

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -26,6 +26,13 @@ export const PATCH = async (req, {params}) => {
         const existingPrompt = await Prompt.findById(params.id)
 
         if(!existingPrompt) return new Response("Prompt Not Found", {status: 404})
+
+        existingPrompt.prompt = prompt
+        existingPrompt.tag = tag
+
+        await existingPrompt.save()
+
+        return new Response(JSON.stringify(existingPrompt), {status: 200})
    } catch (error) {
     
    }
