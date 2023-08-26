147b19bc797b1c68141df698a08ca6ef0162bd4f
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-26 19:54:08 +0000

    Added update prompt page

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+'use client'
 
-const UpdatePrompt = () => {
+import { useSession } from "next-auth/react"
+import { useState } from "react"
+import { useRouter} from "next/navigation"
+import Form from "@components/Form = () => {
+    const router = useRouter();
+
+    const {data: session} = useSession()
+    const [submitting, setSubmitting] = useState(false)
+    const [post, setPost] = useState({
+        prompt: "",
+        tag: "",
+    })
+
+    const createPrompt = async (e) =>{
+        e.preventDefault()
+        setSubmitting(true)
+
+        try{
+            const response = await fetch('/api/prompt/new',{
+                method: 'POST',
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session?.user.id,
+                    tag: post.tag
+                })
+            })
+            if(response.ok){
+                router.push("/")
+            }
+        }catch(error){
+            console.log(error)
+        }finally{
+            setSubmitting(false)
+        }
+    }
+
   return (
-    <div>UpdatePrompt</div>
+    <Form
+    type= "Create"
+    post={post}
+    setPost={setPost}
+    submitting={submitting}
+    handleSubmit={createPrompt}
+    />
   )
 }
 
