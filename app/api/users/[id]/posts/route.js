a76054f7cf75867556a4f3d763dffc685a215189
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-26 16:45:19 +0000

    Working on user posts api

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -7,6 +7,8 @@ export const GET = async (req) =>{
         await connectToDB()
 
         const prompt = await Prompt.find({}).populate('creator')
+
+        return new Response(JSON.stringify(prompt), {status: 200})
     } catch (error) {
         
     }
