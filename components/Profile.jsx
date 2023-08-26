8d11752ef90f49db4489997aeb7f89da16bb29e6
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-26 16:59:55 +0000

    Designing the profile section

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -5,7 +5,8 @@ const Profile = (name, handleDelete, desc, data, handleEdit) => {
   return (
     <section className="w-full">
       <h1>
-      <span className="orange_gradient">My Profile</h1></span>
+      <span className="orange_gradient">My Profile</span>
+      </h1>
     </section>
   )
 }
