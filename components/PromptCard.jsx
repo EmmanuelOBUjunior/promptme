28d249be68bb53e28e055a91d5c282ab36a54716
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-26 18:57:10 +0000

    Designing the profile section

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -54,7 +54,7 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
       </p>
 
       {session?.user.id === post.creator._id && pathName === '/profile' &&(
-        <div>
+        <div className='mt-5 flex-center gap-4'>
           <p className='font-inter text-sm green_gradient cursor-pointer' onClick={handleEdit}>
           Edit
           </p>
