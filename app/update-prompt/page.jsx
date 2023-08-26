8d2f1e8a968e9c934db1eb0070ba0a5f5c17a0d9
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-26 19:56:45 +0000

    Added update prompt page

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useSession } from "next-auth/react"
 import { useState, useEffect } from "react"
 import { useRouter, useSearchParams} from "next/navigation"
 import Form from "@components/Form"
