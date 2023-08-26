5ddde0bb75c3b70450cf81385316146536321df6
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-26 16:46:04 +0000

    Working on user posts api

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -10,6 +10,6 @@ export const GET = async (req) =>{
 
         return new Response(JSON.stringify(prompt), {status: 200})
     } catch (error) {
-        
+       return new Response('Failed to fetch all prompts', {status: 500}) 
     }
 }
