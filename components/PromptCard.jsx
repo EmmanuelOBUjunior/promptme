041f66748168eadbb9edda69d978ea62de780fab
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-26 15:27:10 +0000

    Designing prompt card

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,7 +7,7 @@ import Image from 'next/image'
 
 const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
   return (
-    <div>PromptCard</div>
+    <div className="prompt_car"></div>
   )
 }
 
