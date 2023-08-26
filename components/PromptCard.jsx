5068ca2968e1c9a332399f0ea8f3c2760c9c1c11
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-26 15:50:10 +0000

    Designing prompt card

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -39,10 +39,10 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
         />
       </div>
       </div>
-      <p className='my-4 font-satoshi text-sm'>
+      <p className='my-4 font-satoshi text-sm text-gray-700'>
         {post.prompt}
       </p>
-      <p>
+      <p className='font-inter text-sm amber_gradient cursor-pointer'>
         {post.tag}
       </p>
     </div>
