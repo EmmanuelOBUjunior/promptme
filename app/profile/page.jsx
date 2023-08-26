564b4eb4a384fb4edc357e4b53fe119cb6d8aa80
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-26 16:15:26 +0000

    Creating profie page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react'
-
+import { useState } from 'react'
+import { useSession } from 'next-auth/react'
 const Profile = () => {
   return (
     <div>Profile</div>
