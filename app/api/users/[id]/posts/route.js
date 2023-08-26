c5a3e535c8de3f95457e5082f0cd6a86653dcda0
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-26 16:44:37 +0000

    Working on user posts api

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -5,6 +5,8 @@ import Prompt from "@models/prompt";
 export const GET = async (req) =>{
     try {
         await connectToDB()
+
+        const prompt = await Prompt.find({}).populate('creator')
     } catch (error) {
         
     }
