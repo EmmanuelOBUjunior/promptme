baf5ff789bf6b7abe2a0d69ed12ab314227c4733
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-26 16:21:32 +0000

    Creating profie page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -15,7 +15,7 @@ const MyProfile = () => {
     name='Mine'
     desc ='Welcome to your personalized profile.'
     handleDelete = {}
-    handleEdit = {hande}
+    handleEdit = {handleEdit}
     data = {[]}
     />
   )
