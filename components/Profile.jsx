73891c92657eab3b0b72863c0895765322cb2c81
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-26 17:00:26 +0000

    Designing the profile section

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -4,7 +4,7 @@ import PromptCard from "./PromptCard"
 const Profile = (name, handleDelete, desc, data, handleEdit) => {
   return (
     <section className="w-full">
-      <h1>
+      <h1 className="head_text text-left">
       <span className="orange_gradient">My Profile</span>
       </h1>
     </section>
