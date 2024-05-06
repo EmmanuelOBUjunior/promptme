b7e41c641e44fad4c72d33b7cb3d26a5f757bd97
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-05-06 23:16:02 +0000

    Fixing application client bug

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -20,13 +20,18 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
         <div className='flex-1 flex justify-start items-center gap-3 cursor-pointer'>
-          <Image
+          {
+            post.creator && post.creator.image (
+              <Image
           src={post.creator.image}
           alt='user_image'
           width={40}
           height={40}
           className='rounded-full object-contain'
           />
+            )
+          }
+          
          <div>
           <h3 className='font-satoshi font-semibold text-gray-900'>
             {post.creator.name}
